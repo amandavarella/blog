2d8d8b9b74da16d26a12cb0722f6726dd1fe7d45
Amanda Varella <amanda.varella@envato.com>
2020-07-26 17:45:37 +1000

    Now it can add articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,34 @@
 class ArticlesController < ApplicationController
+    
+    def index
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end 
+
     def new
     end
 
+    def edit
+    end
+
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+
+    #https://guides.rubyonrails.org/getting_started.html#saving-data-in-the-controller
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
     end
 end

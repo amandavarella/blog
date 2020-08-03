5bc3c42a4e69bd656a97d09ae4ca431edf2cbfa5
Amanda Varella <amanda.varella@envato.com>
2020-08-03 18:02:21 +1000

    Adding comments model

--- README.md
+++ README.md
@@ -24,4 +24,4 @@ Things you may want to cover:
 * ...
 # blog
 
-Stopped at https://guides.rubyonrails.org/getting_started.html#adding-a-second-model
+Stopped at https://guides.rubyonrails.org/getting_started.html#generating-a-controller
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :articles
+    resources :comments
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
